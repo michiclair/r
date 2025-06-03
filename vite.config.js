a25a5ee13d56ec758fee7d8aa09e60f327307525
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-03 17:46:58 -0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+﻿import { defineConfig } from 'vite'
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+	base: "./",
+	plugins: [tsconfigPaths(), svgr(), react()],
+});
