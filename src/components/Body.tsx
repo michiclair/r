2ea51bf4f46f723d7c7fa56c460cbd8f0868dbc6
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-04 18:02:19 -0300

    create Body component

---
+++ src/components/Body.tsx
@@ -0,0 +1,12 @@
+﻿import React from "react";
+import "../styles/Body.css";
+
+export default function Body(args: BodyProperties)
+{
+	return <div className="Body">{args.children}</div>
+}
+
+export interface BodyProperties
+{
+	children?: React.ReactNode;
+}
