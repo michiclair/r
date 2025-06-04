7922c97f4390f6e0bfbf8062ebcb63b79fe35594
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-04 18:52:07 -0300

    create App component styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,12 @@
 ﻿import React from "react";
+import "../styles/App.css";
 import Body from "./Body";
+import Header from "./Header.tsx";
 
 export default function App()
 {
-	return <Body></Body>
+	return <div className="App">
+		<Header />
+		<Body />
+	</div>
 }
