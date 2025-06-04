ed8da99d767606c8dee6c4201ec213c569182d29
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-04 18:44:37 -0300

    add main script to webpage

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+﻿import { createRoot } from "react-dom/client";
+import App from "components/App.tsx";
+import { StrictMode } from "react";
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+	<StrictMode>
+		<App></App>
+	</StrictMode>
+);
