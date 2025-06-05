a1851cb48b6c4052024d49648cb24e4340acc168
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-05 17:09:40 -0300

    rewrite ProjectCard to work with ProjectDescriptor

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,26 +1,47 @@
-﻿import React from "react";
+﻿import React, { useEffect, useState } from "react";
 import TechnologyDescriptor from "./TechnologyDescriptor.ts";
 import ImageDescriptor from "./ImageDescriptor.ts";
 import TechnologyTagList from "./TechnologyTagList.tsx";
+import ProjectDescriptor from "./ProjectDescriptor.ts";
+import GithubProjectDescriptor from "./GithubProjectDescriptor.ts";
+import { octokit } from "../shared/octokit.ts";
 
 export default function ProjectCard(props: IProjectCardProperties)
 {
+	const [name, setName] = useState<string>();
+	const [description, setDescription] = useState<string>();
+	const [address, setAddress] = useState<string | undefined>();
+
+	useEffect(() =>
+	{
+		if (props.project instanceof GithubProjectDescriptor)
+		{
+			octokit.rest.repos.get({
+				owner: props.project.ownerName,
+				repo: props.project.repositoryName
+			}).then(response =>
+			{
+				if (response.status < 200 || response.status > 299)
+				{
+					setName("?");
+					setDescription("It appears to be a problem while communicating with GitHub.");
+				}
+
+				setAddress(response.url);
+				setName(response.data.name);
+				setDescription(response.data.description ?? "...");
+			});
+		}
+	}, []);
+
 	return <div className="ProjectCard">
-		{props.image ? <img src={props.image?.source} alt={props.image.alternativeText}/>:null}
-		<div>
-			<a>{props.name}</a>
-			<p>{props.description}</p>
-			<TechnologyTagList technologies={props.technologies}></TechnologyTagList>
-		</div>
+		{address ? <a className="ProjectCard_name" href={address}>{name}</a> : <h1 className="ProjectCard_name">{name}</h1>}
+		<p className="ProjectCard_description">{description}</p>
+		<TechnologyTagList technologies={props.project.technologies}></TechnologyTagList>
 	</div>
 }
 
 export interface IProjectCardProperties
 {
-	name: string;
-	description: string;
-	technologies: readonly TechnologyDescriptor[];
-	image?: ImageDescriptor;
-	demoSource?: string;
-	repositorySource?: string;
+	project: ProjectDescriptor;
 }
