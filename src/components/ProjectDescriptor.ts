59acbb6db8d51e63d98309dedf064f327c018099
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-05 17:08:39 -0300

    Create ProjectDescriptor.ts

---
+++ src/components/ProjectDescriptor.ts
@@ -0,0 +1,16 @@
+﻿import TechnologyDescriptor from "./TechnologyDescriptor.ts";
+
+export default abstract class ProjectDescriptor
+{
+	readonly #_technologies: readonly TechnologyDescriptor[];
+
+	protected constructor(technologies: readonly TechnologyDescriptor[])
+	{
+		this.#_technologies = technologies;
+	}
+
+	public get technologies()
+	{
+		return this.#_technologies;
+	}
+}
