19a9f964b3ca4ae834a59fa39269ea70b7e71636
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-05 14:39:46 -0300

    Create TechnologyTagList.tsx

---
+++ src/components/TechnologyTagList.tsx
@@ -0,0 +1,14 @@
+﻿import React from "react";
+import TechnologyTag from "./TechnologyTag.tsx";
+import TechnologyDescriptor from "./TechnologyDescriptor.ts";
+
+export default function TechnologyTagList(props: ITechnologyTagListProperties)
+{
+	const technologies = props.technologies.map((t, i) => <TechnologyTag key={i} descriptor={t}></TechnologyTag>);
+	return <ul className="TechnologyTagList">{technologies}</ul>;
+}
+
+export interface ITechnologyTagListProperties
+{
+	technologies: readonly TechnologyDescriptor[];
+}
