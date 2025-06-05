8fd8af815109ad2e1f3187136992ae3647a0df7a
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-05 17:31:53 -0300

    update Body component contents

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,12 +1,34 @@
 ﻿import React from "react";
 import "../styles/Body.css";
+import ProjectCard from "./ProjectCard.tsx";
+import TechnologyDescriptor from "./TechnologyDescriptor.ts";
+import ProjectDescriptor from "./ProjectDescriptor.ts";
+import GithubProjectDescriptor from "./GithubProjectDescriptor.ts";
+import ProfilePicture from "./ProfilePicture.tsx";
+import ProjectCardList from "./ProjectCardList.tsx";
 
-export default function Body(args: IBodyProperties)
+export default function Body()
 {
-	return <div className="Body">{args.children}</div>
-}
-
-export interface IBodyProperties
-{
-	children?: React.ReactNode;
+	return <div className="Body">
+		<section className="Body_presentation">
+			<div>
+				<h1>Name</h1>
+				<p>
+					Enthusiastic and dedicated developer with hands-on experience in
+					personal projects and a strong passion for building robust, scalable solutions.
+					Actively seeking my first professional opportunity to apply my
+					skills and grow within a real-world development environment.
+				</p>
+			</div>
+			<ProfilePicture source="https://avatars.githubusercontent.com/u/77251557"></ProfilePicture>
+		</section>
+		<section className="Body_projects">
+			<ProjectCardList projects={[
+				GithubProjectDescriptor.aulaServer,
+				GithubProjectDescriptor.aulajs,
+				GithubProjectDescriptor.enter,
+				GithubProjectDescriptor.aliisRpcMaker,
+			]} />
+		</section>
+	</div>
 }
