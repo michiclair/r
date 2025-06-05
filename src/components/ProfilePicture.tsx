e70f7ccea009762cb56f55991aec0719a51f838a
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-04 22:40:57 -0300

    create ProfilePicture component

---
+++ src/components/ProfilePicture.tsx
@@ -0,0 +1,22 @@
+﻿import React from "react";
+import "../styles/ProfilePicture.css";
+
+export default function ProfilePicture(props: ProfilePictureProperties)
+{
+	return <img
+		className={"ProfilePicture" + (props.className ? ` ${props.className}` : "")}
+		src={props.uri.href}
+		alt="Profile Picture"
+		style={{
+			width: props.width,
+			height: props.height,
+		}}/>
+}
+
+export interface ProfilePictureProperties
+{
+	className?: string;
+	uri: URL;
+	width?: string;
+	height?: string;
+}
