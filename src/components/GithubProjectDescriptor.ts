2a8267048d4a76c6b5b359f2cae9892ef923fcd7
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-05 17:31:28 -0300

    add default GitHubProjectDescriptor instances

--- src/components/GithubProjectDescriptor.ts
+++ src/components/GithubProjectDescriptor.ts
@@ -3,14 +3,76 @@ import TechnologyDescriptor from "./TechnologyDescriptor.ts";
 
 export default class GithubProjectDescriptor extends ProjectDescriptor
 {
+	static readonly #s_aulajs: GithubProjectDescriptor = new GithubProjectDescriptor(
+		[
+			TechnologyDescriptor.javascript,
+			TechnologyDescriptor.typescript,
+			TechnologyDescriptor.nodejs,
+		],
+		"michironoaware",
+		"aula.js");
+	static readonly #s_aulaServer: GithubProjectDescriptor = new GithubProjectDescriptor(
+		[
+			TechnologyDescriptor.dotnet,
+			TechnologyDescriptor.aspnet,
+			TechnologyDescriptor.csharp,
+			TechnologyDescriptor.efcore,
+			TechnologyDescriptor.docker,
+		],
+		"michironoaware",
+		"Aula-Server");
+	static readonly #s_enter: GithubProjectDescriptor = new GithubProjectDescriptor(
+		[
+			TechnologyDescriptor.javascript,
+			TechnologyDescriptor.typescript,
+			TechnologyDescriptor.nodejs,
+			TechnologyDescriptor.html,
+			TechnologyDescriptor.css,
+			TechnologyDescriptor.react,
+		],
+		"michironoaware",
+		"ENTER");
+	static readonly #s_aliisRpcMaker: GithubProjectDescriptor = new GithubProjectDescriptor(
+		[
+			TechnologyDescriptor.javascript,
+			TechnologyDescriptor.typescript,
+			TechnologyDescriptor.nodejs,
+			TechnologyDescriptor.html,
+			TechnologyDescriptor.css,
+			TechnologyDescriptor.tailwind,
+			TechnologyDescriptor.electron
+		],
+		"michironoaware",
+		"AliisRPCMaker");
+
 	readonly #_ownerName: string;
 	readonly #_repositoryName: string;
 
-	public constructor(technologies: readonly TechnologyDescriptor[], ownerName: string, repositoryName: string)
+	private constructor(technologies: readonly TechnologyDescriptor[], ownerName: string, repositoryName: string)
 	{
 		super(technologies);
 		this.#_ownerName = ownerName;
 		this.#_repositoryName = repositoryName;
+	}
+
+	public static get aulajs()
+	{
+		return this.#s_aulajs;
+	}
+
+	public static get aulaServer()
+	{
+		return this.#s_aulaServer;
+	}
+
+	public static get enter()
+	{
+		return this.#s_enter;
+	}
+
+	public static get aliisRpcMaker()
+	{
+		return this.#s_aliisRpcMaker;
 	}
 
 	public get ownerName()
