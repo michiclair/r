eeda6ed5c747f00ea8fe2ce1810c144697b6918a
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-05 17:09:43 -0300

    Create ProjectCardList.tsx

---
+++ src/components/ProjectCardList.tsx
@@ -0,0 +1,15 @@
+﻿import React from "react";
+import ProjectCard, { IProjectCardProperties } from "./ProjectCard.tsx";
+import TechnologyDescriptor from "./TechnologyDescriptor.ts";
+import ProjectDescriptor from "./ProjectDescriptor.ts";
+
+export default function ProjectCardList(props: IProjectCardListProperties)
+{
+	const projectCards = props.projects.map((p, i) => <ProjectCard key={i} project={p}></ProjectCard>);
+	return <ul className="ProjectCardList">{projectCards}</ul>;
+}
+
+export interface IProjectCardListProperties
+{
+	projects: readonly ProjectDescriptor[];
+}
