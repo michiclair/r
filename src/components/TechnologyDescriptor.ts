ad402adaae135dabd2427c49b82e2490da1a1f5d
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-05 14:39:36 -0300

    Create TechnologyDescriptor.ts

---
+++ src/components/TechnologyDescriptor.ts
@@ -0,0 +1,30 @@
+﻿export default class TechnologyDescriptor
+{
+	static readonly #s_csharp: TechnologyDescriptor = new TechnologyDescriptor(
+		"C#",
+		"https://dotnet.microsoft.com/en-us/languages/csharp");
+
+	readonly #_name: string;
+	readonly #_source: string;
+
+	private constructor(name: string, source: string)
+	{
+		this.#_name = name;
+		this.#_source = source;
+	}
+
+	public static get csharp()
+	{
+		return this.#s_csharp;
+	}
+
+	public get name()
+	{
+		return this.#_name;
+	}
+
+	public get source()
+	{
+		return this.#_source;
+	}
+}
