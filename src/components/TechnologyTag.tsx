febeceb1757bb035e01a491876f1bc9eb232ec28
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-05 14:39:42 -0300

    Create TechnologyTag.tsx

---
+++ src/components/TechnologyTag.tsx
@@ -0,0 +1,11 @@
+﻿import TechnologyDescriptor from "./TechnologyDescriptor.ts";
+
+export default function TechnologyTag(props: ITechnologyTagProperties)
+{
+	return <a className="TechnologyTag" href={props.descriptor.source}>{props.descriptor.name}</a>;
+}
+
+export interface ITechnologyTagProperties
+{
+	descriptor: TechnologyDescriptor;
+}
