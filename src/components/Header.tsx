de7db7a448b3e1baad1c30ac0d64914543d9757e
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-04 22:41:57 -0300

    update Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,5 +4,9 @@ import "../styles/Header.css";
 export default function Header()
 {
 	return <header className="Header">
+		<div className="leftSide">
+		</div>
+		<div className="rightSide">
+		</div>
 	</header>
 }
