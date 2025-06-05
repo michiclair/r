27826eb27243ee5f77e7fc5823d1e7a05120c7d4
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-05 17:08:41 -0300

    Create GithubProjectDescriptor.ts

---
+++ src/components/GithubProjectDescriptor.ts
@@ -0,0 +1,25 @@
+﻿import ProjectDescriptor from "./ProjectDescriptor.ts";
+import TechnologyDescriptor from "./TechnologyDescriptor.ts";
+
+export default class GithubProjectDescriptor extends ProjectDescriptor
+{
+	readonly #_ownerName: string;
+	readonly #_repositoryName: string;
+
+	public constructor(technologies: readonly TechnologyDescriptor[], ownerName: string, repositoryName: string)
+	{
+		super(technologies);
+		this.#_ownerName = ownerName;
+		this.#_repositoryName = repositoryName;
+	}
+
+	public get ownerName()
+	{
+		return this.#_ownerName;
+	}
+
+	public get repositoryName()
+	{
+		return this.#_repositoryName;
+	}
+}
