667b59b57481d8ef8dc6cb9aa9df548a00399a08
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-05 14:39:34 -0300

    Create ImageDescriptor.ts

---
+++ src/components/ImageDescriptor.ts
@@ -0,0 +1,21 @@
+﻿export default class ImageDescriptor
+{
+	readonly #_source: string;
+	readonly #_alternativeText: string;
+
+	private constructor(source: string, alternativeText: string)
+	{
+		this.#_source = source;
+		this.#_alternativeText = alternativeText;
+	}
+
+	public get source()
+	{
+		return this.#_source;
+	}
+
+	public get alternativeText()
+	{
+		return this.#_alternativeText;
+	}
+}
