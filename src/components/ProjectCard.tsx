f2fb72d85f8e97e97bd315334203c02c7f4a5ac2
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-05 14:39:50 -0300

    Create ProjectCard.tsx

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,26 @@
+﻿import React from "react";
+import TechnologyDescriptor from "./TechnologyDescriptor.ts";
+import ImageDescriptor from "./ImageDescriptor.ts";
+import TechnologyTagList from "./TechnologyTagList.tsx";
+
+export default function ProjectCard(props: IProjectCardProperties)
+{
+	return <div className="ProjectCard">
+		{props.image ? <img src={props.image?.source} alt={props.image.alternativeText}/>:null}
+		<div>
+			<a>{props.name}</a>
+			<p>{props.description}</p>
+			<TechnologyTagList technologies={props.technologies}></TechnologyTagList>
+		</div>
+	</div>
+}
+
+export interface IProjectCardProperties
+{
+	name: string;
+	description: string;
+	technologies: readonly TechnologyDescriptor[];
+	image?: ImageDescriptor;
+	demoSource?: string;
+	repositorySource?: string;
+}
