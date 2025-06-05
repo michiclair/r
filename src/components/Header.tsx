d2b8827404d06d2c1e35074310727125a9f735ef
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-04 22:42:55 -0300

    add profile picture to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 ﻿import React from "react";
 import "../styles/Header.css";
+import ProfilePicture from "./ProfilePicture";
 
 export default function Header()
 {
 	return <header className="Header">
 		<div className="leftSide">
+			<ProfilePicture
+				uri={new URL("https://avatars.githubusercontent.com/u/77251557")}
+				className="profilePicture"/>
 		</div>
 		<div className="rightSide">
 		</div>
