16c828d93b2cb3b86795f1a9673513a9b30c7c36
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-05 21:15:44 -0300

    create StackCard component

---
+++ src/components/StackCard.tsx
@@ -0,0 +1,18 @@
+﻿import TechnologyDescriptor from "./TechnologyDescriptor.ts";
+import "../styles/StackCard.css";
+
+export default function StackCard(props: IStackCardProperties)
+{
+	const technologies = props.technologies.map(t =>
+			<a href={t.source}><img src={t.imageSource} alt={t.name} /></a>);
+
+	return <div className="StackCard">
+		<h3>{props.name}</h3>
+		<div className="StackCard_technologies">{technologies}</div>
+	</div>
+}
+
+export interface IStackCardProperties {
+	name: string;
+	technologies: readonly TechnologyDescriptor[];
+}
