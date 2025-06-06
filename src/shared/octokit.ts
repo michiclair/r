761347d1acdfa92de43260ec2e4d958e631a364f
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-05 21:54:57 -0300

    add note about the exposed token

--- src/shared/octokit.ts
+++ src/shared/octokit.ts
@@ -1,5 +1,6 @@
 ﻿import { Octokit } from "octokit";
 
+// This GitHub token is read-only and does not pose a security risk.
 export const octokit = new Octokit({
 	auth: "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"
 });
