5583ad35d97ca38d6daccfeedf598fb806e6aa9f
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-06 15:43:03 -0300

    create TechnologyButton component

---
+++ src/components/TechnologyButton.tsx
@@ -0,0 +1,14 @@
+﻿import TechnologyDescriptor from "./TechnologyDescriptor.ts";
+import "../styles/TechnologyButton.css";
+
+export default function TechnologyButton(props: ITechnologyButtonProperties)
+{
+	return <a className="TechnologyButton" href={props.technology.source}>
+		<img src={props.technology.imageSource} alt={props.technology.name}/>
+	</a>
+}
+
+export interface ITechnologyButtonProperties
+{
+	technology: TechnologyDescriptor;
+}
