7ced18b69ab8374dd723f2f7b8dc3df4370f82cb
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-06 18:05:27 -0300

    create SocialIconList component

---
+++ src/components/SocialIconList.tsx
@@ -0,0 +1,13 @@
+﻿import SocialIcon, { ISocialIconProperties } from "./SocialIcon.tsx";
+import "../styles/SocialIconList.css";
+
+export default function SocialIconList(props: ISocialIconListProperties)
+{
+	const socials = props.socials.map(s => <SocialIcon iconSource={s.iconSource} name={s.name} profileAddress={s.profileAddress}/>);
+	return <div className="SocialIconList">{socials}</div>
+}
+
+export interface ISocialIconListProperties
+{
+	socials: readonly ISocialIconProperties[];
+}
