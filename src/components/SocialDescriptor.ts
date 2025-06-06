95209cc00a59a78f196c1116b28a0bb947b8a64d
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-06 18:32:02 -0300

    create base SocialDescriptor class

---
+++ src/components/SocialDescriptor.ts
@@ -0,0 +1,21 @@
+﻿export abstract class SocialDescriptor
+{
+	readonly #_name: string;
+	readonly #_iconSource: string;
+
+	protected constructor(name: string, iconSource: string)
+	{
+		this.#_name = name;
+		this.#_iconSource = iconSource;
+	}
+
+	public get name()
+	{
+		return this.#_name;
+	}
+
+	public get iconSource()
+	{
+		return this.#_iconSource;
+	}
+}
