ad7469bedce370955101aff6c06d9bcbcc8ce340
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-06 18:32:09 -0300

    create RedirectSocialDescriptor class

---
+++ src/components/RedirectSocialDescriptor.ts
@@ -0,0 +1,36 @@
+﻿import { SocialDescriptor } from "./SocialDescriptor.ts";
+
+export class RedirectSocialDescriptor extends SocialDescriptor
+{
+	static readonly #s_github: RedirectSocialDescriptor = new RedirectSocialDescriptor(
+		"GitHub",
+		"/assets/github_icon.svg",
+		"https://github.com/michironoaware");
+	static readonly #s_linkedin: RedirectSocialDescriptor = new RedirectSocialDescriptor(
+		"LinkedIn",
+		"/assets/linkedin_icon.svg",
+		"");
+
+	readonly #_address: string;
+
+	public constructor(name: string, iconSource: string, address: string)
+	{
+		super(name, iconSource);
+		this.#_address = address;
+	}
+
+	public static get github()
+	{
+		return this.#s_github;
+	}
+
+	public static get linkedin()
+	{
+		return this.#s_linkedin;
+	}
+
+	public get address()
+	{
+		return this.#_address;
+	}
+}
