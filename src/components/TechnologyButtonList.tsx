48fd6fe7afdfd44060d8983a1149ca5bfcfec8d6
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-06 15:43:11 -0300

    create TechnologyButtonList component

---
+++ src/components/TechnologyButtonList.tsx
@@ -0,0 +1,14 @@
+﻿import TechnologyDescriptor from "./TechnologyDescriptor.ts";
+import TechnologyButton from "./TechnologyButton.tsx";
+import "../styles/TechnologyButtonList.css";
+
+export default function TechnologyButtonList(props: ITechnologyButtonListProperties)
+{
+	const technologies = props.technologies.map(t => <TechnologyButton technology={t}/>);
+	return <div className="TechnologyButtonList">{technologies}</div>
+}
+
+export interface ITechnologyButtonListProperties
+{
+	technologies: readonly TechnologyDescriptor[];
+}
