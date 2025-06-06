fd8adfcd93f0ac3346d732837a7b6b8e0af1e60b
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-06 20:16:30 -0300

    remove github access token

--- src/shared/octokit.ts
+++ src/shared/octokit.ts
@@ -1,6 +1,3 @@
 ﻿import { Octokit } from "octokit";
 
-// This GitHub token is read-only and does not pose a security risk.
-export const octokit = new Octokit({
-	auth: "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"
-});
+export const octokit = new Octokit();
