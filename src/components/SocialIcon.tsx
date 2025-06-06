962c7cb430948a44a708ea77d47ac4835b9e88a8
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-06 18:49:58 -0300

    add SocialIcon support for ClipboardSocialDescriptor

--- src/components/SocialIcon.tsx
+++ src/components/SocialIcon.tsx
@@ -1,14 +1,35 @@
 ﻿import "../styles/SocialIcon.css";
 import { SocialDescriptor } from "./SocialDescriptor.ts";
 import { RedirectSocialDescriptor } from "./RedirectSocialDescriptor.ts";
+import { ClipboardSocialDescriptor } from "./ClipboardSocialDescriptor.ts";
+import { useEffect, useId } from "react";
 
 export default function SocialIcon(props: ISocialIconProperties)
 {
 	if (props.social instanceof RedirectSocialDescriptor)
 	{
-		return <a className="SocialIcon" href={props.social.address}>
+		return <a className="SocialIcon RedirectSocialIcon" href={props.social.address}>
 			<img src={props.social.iconSource} alt={props.social.name}/>
 		</a>
+	}
+	else if (props.social instanceof ClipboardSocialDescriptor)
+	{
+		const elementId = useId();
+
+		useEffect(() =>
+		{
+			const copyToClipboardCallback = () =>
+				navigator.clipboard.writeText((props.social as ClipboardSocialDescriptor).content).then();
+
+			const socialIconElement = document.getElementById(elementId)!;
+			socialIconElement.addEventListener("click", copyToClipboardCallback);
+
+			return () => socialIconElement.removeEventListener("click", copyToClipboardCallback);
+		}, []);
+
+		return <div id={elementId} className="SocialIcon">
+			<img src={props.social.iconSource} alt={props.social.name}/>
+		</div>
 	}
 }
 
