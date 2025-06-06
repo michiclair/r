c9fce2975415bf913062dc93ffe26a1350c751e3
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-06 18:05:14 -0300

    create SocialIcon component

---
+++ src/components/SocialIcon.tsx
@@ -0,0 +1,15 @@
+﻿import "../styles/SocialIcon.css";
+
+export default function SocialIcon(props: ISocialIconProperties)
+{
+	return <a className="SocialIcon" href={props.profileAddress}>
+		<img src={props.iconSource} alt={props.name}/>
+	</a>
+}
+
+export interface ISocialIconProperties
+{
+	iconSource: string;
+	name: string;
+	profileAddress: string;
+}
