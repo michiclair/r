b2565743bbb56f032f023c5cf69f83d88690840d
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-06-06 18:49:40 -0300

    create ClipboardSocialDescriptor

---
+++ src/components/ClipboardSocialDescriptor.ts
@@ -0,0 +1,36 @@
+﻿import { SocialDescriptor } from "./SocialDescriptor.ts";
+
+export class ClipboardSocialDescriptor extends SocialDescriptor
+{
+	static readonly #_email: ClipboardSocialDescriptor = new ClipboardSocialDescriptor(
+		"Email",
+		"/assets/email_icon.svg",
+		"michironoaware@gmail.com");
+	static readonly #_phone: ClipboardSocialDescriptor = new ClipboardSocialDescriptor(
+		"Phone",
+		"/assets/phone_icon.svg",
+		"0");
+
+	readonly #_content: string;
+
+	public constructor(name: string, iconSource: string, content: string)
+	{
+		super(name, iconSource);
+		this.#_content = content;
+	}
+
+	public get content()
+	{
+		return this.#_content;
+	}
+
+	public static get email()
+	{
+		return this.#_email;
+	}
+
+	public static get phone()
+	{
+		return this.#_phone;
+	}
+}
