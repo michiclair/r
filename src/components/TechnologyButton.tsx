8befeceefbc64c5d1a10779dabd0505eda055bf4
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-07-15 22:20:23 -0300

    fix!: redirect user when clicking a technology button

--- src/components/TechnologyButton.tsx
+++ src/components/TechnologyButton.tsx
@@ -1,11 +1,27 @@
 ﻿import TechnologyDescriptor from "./TechnologyDescriptor.ts";
 import "../styles/TechnologyButton.css";
+import { useEffect, useId } from "react";
+import { RedirectSocialDescriptor } from "./RedirectSocialDescriptor.ts";
 
 export default function TechnologyButton(props: ITechnologyButtonProperties)
 {
-	return <a className="TechnologyButton Button" href={props.technology.source} target="_blank" rel="noopener noreferrer">
+	// We use an event listener instead of an <a> tag because react-chrono's timeline overrides the default behavior of <a> tags. (bug)
+	const elementId = useId();
+
+	useEffect(() =>
+	{
+		const redirectCallback = () =>
+			window.open(props.technology.source);
+
+		const socialIconElement = document.getElementById(elementId)!;
+		socialIconElement.addEventListener("click", redirectCallback);
+
+		return () => socialIconElement.removeEventListener("click", redirectCallback);
+	}, []);
+
+	return <button className="TechnologyButton Button" id={elementId}>
 		<img src={props.technology.imageSource} alt={props.technology.name}/>
-	</a>
+	</button>
 }
 
 export interface ITechnologyButtonProperties
