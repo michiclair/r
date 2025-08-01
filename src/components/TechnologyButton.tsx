779aab20e2003b387f3011155e324eace8788757
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-08-01 02:17:59 -0300

    honestly idc about building a good commit history for this repository

--- src/components/TechnologyButton.tsx
+++ src/components/TechnologyButton.tsx
@@ -1,28 +1,13 @@
 ﻿import TechnologyDescriptor from "./TechnologyDescriptor.ts";
 import "../styles/TechnologyButton.css";
-import { useEffect, useId } from "react";
-import { RedirectSocialDescriptor } from "./RedirectSocialDescriptor.ts";
 
 export default function TechnologyButton(props: ITechnologyButtonProperties)
 {
 	// We use an event listener instead of an <a> tag because react-chrono's timeline overrides the default behavior of <a> tags. (bug)
-	const elementId = useId();
-
-	useEffect(() =>
-	{
-		const redirectCallback = () =>
-			window.open(props.technology.source);
-
-		const socialIconElement = document.getElementById(elementId)!;
-		socialIconElement.addEventListener("click", redirectCallback);
-
-		return () => socialIconElement.removeEventListener("click", redirectCallback);
-	}, []);
-
-	return <button className="TechnologyButton Button" id={elementId}>
+	return <a className="TechnologyButton Button" href={props.technology.source} target="_blank" rel="noopener noreferrer">
 		{props.technology.name}
 		<img src={props.technology.imageSource} alt={props.technology.name}/>
-	</button>
+	</a>
 }
 
 export interface ITechnologyButtonProperties
